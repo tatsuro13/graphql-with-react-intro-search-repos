bd4f9af65a028ca34f52f67bb78041ffc3703438
tatsuro13 <oscarwild13@gmail.com>
2019-12-12 17:16:09 +0900

    setup dotenv ad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-console.log(GITHUB_TOKEN)
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+console.log(GITHUB_TOKEN);
 
 function App() {
-  return (
-    <div>Hello, GraphQL</div>
-  );
+  return <div>Hello, GraphQL</div>;
 }
 
 export default App;
