eeb63ee10b7faa9b695290d872de2ac58d061af6
tatsuro13 <oscarwild13@gmail.com>
2019-12-17 17:08:32 +0900

    setup grapgQL client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import {ApolloProvider} from 'react-apollo';
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import client from './client';
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello, GraphQL</div>
-    </ApolloProvider>
-  );
+const ME = gql`
+  query me {
+    user(login: "tatsuro13") {
+      name
+      avatarUrl
+    }
+  }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello, GraphQL</div>
+
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`;
+
+            return <div>{data.user.name}</div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
--- src/client.js
+++ src/client.js
@@ -1,24 +1,24 @@
-import {ApolloClient} from 'apollo-client'
-import {ApolloLink} from 'apollo-link'
-import {HttpLink} from 'apollo-link-http'
-import {InMemoryCache} from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const headersLink = new ApolloLink((operation, forward) => {
-    operation.setContext({
-        headers: {
-            Authorization: `Bearer ${GITHUB_TOKEN}`
-        }
-    })
-    return forward(operation)
-})
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`
+    }
+  });
+  return forward(operation);
+});
 
-const endpoint = 'https://api.github.com/graphql'
-const httpLink = new HttpLink({uri: endpoint})
-const link = ApolloLink.from([headersLink, httpLink])
+const endpoint = 'https://api.github.com/graphql';
+const httpLink = new HttpLink({ uri: endpoint });
+const link = ApolloLink.from([headersLink, httpLink]);
 
 export default new ApolloClient({
-    link,
-    cache: InMemoryCache()
-})
+  link,
+  cache: new InMemoryCache()
+});
