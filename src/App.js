6e66994fd5bec6d12cadf0f4bc871779f9d899c3
tatsuro13 <oscarwild13@gmail.com>
2019-12-17 17:42:47 +0900

    separete graphql code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import client from './client';
-
-const ME = gql`
-  query me {
-    user(login: "tatsuro13") {
-      name
-      avatarUrl
-    }
-  }
-`;
+import { ME } from './graphql';
 
 class App extends Component {
   render() {
