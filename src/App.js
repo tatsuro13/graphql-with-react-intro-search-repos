5838b7471a700103eb2fd6601a2403f852a2b183
tatsuro13 <oscarwild13@gmail.com>
2019-12-17 17:10:41 +0900

    setup grapgQL client
    # Conflicts:
    #	src/App.js
    #	src/client.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import client from './client';
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log(GITHUB_TOKEN);
+const ME = gql`
+  query me {
+    user(login: "tatsuro13") {
+      name
+      avatarUrl
+    }
+  }
+`;
 
-function App() {
-  return <div>Hello, GraphQL</div>;
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello, GraphQL</div>
+
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`;
+
+            return <div>{data.user.name}</div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
