fe318fbe724337608d11288583c79636a7d8856a
tatsuro13 <oscarwild13@gmail.com>
2019-12-22 14:05:05 +0900

    remove-star
    (cherry picked from commit c38b575bfbe1966b2ed77b767ffc762e9eaff4ea)

--- src/graphql.js
+++ src/graphql.js
@@ -51,6 +51,18 @@ mutation addStar ($input: AddStarInput!) {
   }
 }
 `
+
+export const REMOVE_STAR = gql`
+mutation removeStar ($input: RemoveStarInput!) {
+  removeStar (input: $input) {
+    starrable {
+      id
+      viewerHasStarred
+    }
+  }
+}
+`
+
 export const ME = gql`
   query me {
     user(login: "tatsuro13") {
